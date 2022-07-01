c90863cb3675096fa6c9831070d66cbc9af4753b
enkoding1 <ksjdevelop53@gmail.com>
2022-07-01 23:48:59 +0900

    working

--- js/job/app.js
+++ js/job/app.js
@@ -4,7 +4,7 @@ let label = ['sun','mon','tue','wed','thi','fri','sat'];
 let data = [1,2,3,4,5,6,7];
 
 let config = {
-    type: 'bar',
+    type: 'line',
     // ②차트의 데이터(Object)
     data: {
         // ③x축에 들어갈 이름들(Array)
@@ -41,3 +41,44 @@ window.onload = ()=>{
     console.log(config);
     new Chart(chart,config)
 }
+
+
+// new Chart(document.getElementById("myChart"), {
+//     type: 'line',
+//     data: {
+//       labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+//       datasets: [{ 
+//           data: [86,114,106,106,107,111,133,221,783,2478],
+//           label: "Africa",
+//           borderColor: "#3e95cd",
+//           fill: false
+//         }, { 
+//           data: [282,350,411,502,635,809,947,1402,3700,5267],
+//           label: "Asia",
+//           borderColor: "#8e5ea2",
+//           fill: false
+//         }, { 
+//           data: [168,170,178,190,203,276,408,547,675,734],
+//           label: "Europe",
+//           borderColor: "#3cba9f",
+//           fill: false
+//         }, { 
+//           data: [40,20,10,16,24,38,74,167,508,784],
+//           label: "Latin America",
+//           borderColor: "#e8c3b9",
+//           fill: false
+//         }, { 
+//           data: [6,3,2,2,7,26,82,172,312,433],
+//           label: "North America",
+//           borderColor: "#c45850",
+//           fill: false
+//         }
+//       ]
+//     },
+//     options: {
+//       title: {
+//         display: true,
+//         text: 'World population per region (in millions)'
+//       }
+//     }
+//   });
