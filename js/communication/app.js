7d578169f3b486424b0991232698d88da4583c6a
unknown <ksjdevelop53@gmail.com>
2022-07-13 23:01:20 +0900

    test: cors

--- js/communication/app.js
+++ js/communication/app.js
@@ -1,44 +1,53 @@
 // const url = 'https://jsonplaceholder.typicode.com/posts';
-// const method = 'get';
-// let data = [];
-// let option = {
-//     method: 'POST',
-//     headers: { 
-//         "Content-Type": "application/json" 
-//     },
-//     body:JSON.stringify({
-//         //보내고 싶은 데이터 넣으면 됨
-//     })
+function run(){
+  const url = 'http://my.parkjeongseop.com:8000/api/accounts/v1/registration/';
+const method = 'POST';
+let data = [];
 
-// }
+//username, password1, password2 ,
+let option = {
+    method: 'POST',
+    headers: { 
+        "Content-Type": "application/json" 
+    },
+    body:JSON.stringify({
+         username:'hellopark',
+         password1:'googlechrome',
+         password2:'googlechrome'
+    })
 
+}
 
-// fetch(url)
-//     .then(response => {
-//         return response.json();
-//     }) 
-//     .then(post=>{
-//         console.log(post.userId)
-//     })   
-//     .catch(err => {
-//         console.error(err)
-//     })
-//     // .then(data =>{
-//     //     console.log(data)
-//     // })
+ 
+
+fetch(url,option)
+    .then(response => {
+       console.log('success!!',response)  
+      // return response.json();
+    })   
+    .catch(err => {
+        console.error('error!!!',err)
+    })
+    // .then(data =>{
+    //     console.log(data)
+    // })
+}
     
 
 
+ 
 
-function fetchAuthorName(postId) {
-    return fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
-      .then((response) => response.json())
-      .then((post) => post.userId)
-      .then((userId) => {
-        return fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
-          .then((response) => response.json())
-          .then((user) => user.name);
-      });
-  }
+
+
+// function fetchAuthorName(postId) {
+//     return fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+//       .then((response) => response.json())
+//       .then((post) => post.userId)
+//       .then((userId) => {
+//         return fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+//           .then((response) => response.json())
+//           .then((user) => user.name);
+//       });
+//   }
   
-  fetchAuthorName(1).then((name) => console.log("name:", name));
+//   fetchAuthorName(1).then((name) => console.log("name:", name));
--- login/js/login.js
+++ login/js/login.js
@@ -16,17 +16,17 @@ function register() {
              
         <div class="id_box">
             <img class="loginimg" src="./media/id.png" alt="">
-            <input class="input" type="text" placeholder="Id" name="" id="">
+            <input class="input" type="text" placeholder="Id" name="username">
         </div>
 
         <div class="pw_box">
             <img class="loginimg" src="./media/password.png" alt="">
-        <input class="input" type="password" placeholder="Password" name="" id="">
+        <input class="input" type="password" placeholder="Password" name="password1">
         </div>
 
         <div class="pw_chekbox">
             <img class="loginimg" src="./media/checkpw.png" alt="">
-        <input class="input" type="password" placeholder="Password" name="" id="">
+        <input class="input" type="password" placeholder="Password" name="password2">
         </div>
         <button class="submit">
             가입하기
@@ -44,11 +44,11 @@ function signin() {
          
     <div class="id_box">
         <img class="loginimg" src="./media/id.png" alt="">
-        <input class="input" type="text" placeholder="Id" name="" id="">
+        <input class="input" type="text" placeholder="Id" name="" >
     </div>
     <div class="pw_box">
         <img class="loginimg" src="./media/password.png" alt="">
-    <input class="input" type="text" placeholder="Password" name="" id="">
+    <input class="input" type="text" placeholder="Password" name="">
     </div>
     <button class="submit">
         login
