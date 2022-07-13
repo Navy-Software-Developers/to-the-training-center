988951903c9b5ff80c9d1efa87229a9e0a047a89
unknown <ksjdevelop53@gmail.com>
2022-07-13 23:59:09 +0900

    test:login

--- login/js/login.js
+++ login/js/login.js
@@ -1,17 +1,7 @@
-// https://www.flaticon.com/kr/free-icons/login      icon license
-const submit_btn = document.querySelector('.submit');
-const imgSrc = {
-    id: './media/id.png',
-    pw: './media/password.png',
-    checkpw: './media/checkpw.png'
-};
-
-const loginContent = document.querySelector('.content');
-
+const loginContent = document.querySelector(".content");
 
 function register() {
-    loginContent.innerHTML =
-        `
+  loginContent.innerHTML = `
         <h1 class="login_title">Register</h1>
              
         <div class="id_box">
@@ -21,25 +11,24 @@ function register() {
 
         <div class="pw_box">
             <img class="loginimg" src="./media/password.png" alt="">
-        <input class="input" type="password" placeholder="Password" name="password1">
+        <input class="input pw1" type="password" placeholder="Password" name="password1">
         </div>
 
         <div class="pw_chekbox">
             <img class="loginimg" src="./media/checkpw.png" alt="">
-        <input class="input" type="password" placeholder="Password" name="password2">
+        <input class="input pw2" type="password" placeholder="Password" name="password2">
         </div>
         <button class="submit">
             가입하기
         </button>
 
-        `
+        `;
 
-        return ;
+  return;
 }
 
 function signin() {
-    loginContent.innerHTML =
-    `
+  loginContent.innerHTML = `
     <h1 class="login_title">LOGIN</h1>
          
     <div class="id_box">
@@ -48,7 +37,7 @@ function signin() {
     </div>
     <div class="pw_box">
         <img class="loginimg" src="./media/password.png" alt="">
-    <input class="input" type="text" placeholder="Password" name="">
+    <input class="input password" type="text" placeholder="Password" name="">
     </div>
     <button class="submit">
         login
@@ -57,17 +46,64 @@ function signin() {
     <div class="register">
         가입하기
     </div> 
-    `
+    `;
 }
- 
-let click = false;
 
-window.addEventListener('click',()=>{
-    if(click == false){
+signin();
+window.onload = () => {
+  // https://www.flaticon.com/kr/free-icons/login      icon license
+  const submit_btn = document.querySelector(".submit");
+  const imgSrc = {
+    id: "./media/id.png",
+    pw: "./media/password.png",
+    checkpw: "./media/checkpw.png",
+  };
+
+
+
+  let password = document.querySelector('.password');
+  let joinpw1 = null;
+  let joinpw2 = null;
+ 
+  const join = document.querySelector('.register')
+  
+  let click = false;
+  
+
+
+
+  let regisetClick = ()=>{
+    if (click == false) {
         register();
+        joinpw1 = document.querySelector('.pw1');
+        joinpw2 = document.querySelector('.pw2');
+        joinpw2.addEventListener('keyup',checkPw)
         click = true;
-    }else if(click == true){
-        signin()
+      } else if (click == true) {
+        signin();
         click = false;
-    }
-})
+      }
+  }
+ 
+  let checkPw = ()=>{
+      const joinpw1_data = joinpw1.value;
+      const joinpw2_data = joinpw2.value;
+    
+      if(joinpw1_data == joinpw2_data){
+        console.log('same');
+   
+      }else if(
+        joinpw1_data != joinpw2_data){
+            joinpw2.style.border = `1px solid rgba(200,1,1,0.5)`;
+            
+        }
+      
+      
+  }
+
+  join.addEventListener("click", regisetClick);
+
+ 
+
+
+};
