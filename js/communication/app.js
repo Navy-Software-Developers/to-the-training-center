53b114405b4e4e2d11cf93f05934147ed8960ae2
enkoding1 <ksjdevelop53@gmail.com>
2022-07-13 17:02:42 +0900

    test: test fetch

--- js/communication/app.js
+++ js/communication/app.js
@@ -1,3 +1,44 @@
-xhr = new XMLHttpRequest();
-xhr.open('post','https://naver.com');
-console.log(xhr.status);
+// const url = 'https://jsonplaceholder.typicode.com/posts';
+// const method = 'get';
+// let data = [];
+// let option = {
+//     method: 'POST',
+//     headers: { 
+//         "Content-Type": "application/json" 
+//     },
+//     body:JSON.stringify({
+//         //보내고 싶은 데이터 넣으면 됨
+//     })
+
+// }
+
+
+// fetch(url)
+//     .then(response => {
+//         return response.json();
+//     }) 
+//     .then(post=>{
+//         console.log(post.userId)
+//     })   
+//     .catch(err => {
+//         console.error(err)
+//     })
+//     // .then(data =>{
+//     //     console.log(data)
+//     // })
+    
+
+
+
+function fetchAuthorName(postId) {
+    return fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+      .then((response) => response.json())
+      .then((post) => post.userId)
+      .then((userId) => {
+        return fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+          .then((response) => response.json())
+          .then((user) => user.name);
+      });
+  }
+  
+  fetchAuthorName(1).then((name) => console.log("name:", name));
