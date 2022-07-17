52a15b2c3df7f23851aadcfa1a951be3d21df372
enkoding1 <ksjdevelop53@gmail.com>
2022-07-17 10:28:44 +0900

    fix : shopping mall js

--- mall/js/app.js
+++ mall/js/app.js
@@ -6,11 +6,14 @@ class App {
     this.product_data = product_data;
     this.main = document.querySelector(".main");
     this.showItem = document.querySelector(".show_item");
-
     this.totalPrice = document.querySelector(".now_price");
     this.priceCalc = 0;
     this.purchase = document.querySelector(".purchase");
-    
+
+    this.priceBox = document.querySelector(".price_box");
+
+    this.priceBox.style.right = `${-this.priceBox.offsetWidth}px`;
+    this.priceBox.style.display = "none";
     this.count = 0;
 
     //제품 목록
@@ -22,9 +25,9 @@ class App {
 
     this.createItem();
     this.clickEvent();
-    
-    this.purchase.addEventListener('click',this.purchaseAlert.bind(this));
-}
+
+    this.purchase.addEventListener("click", this.purchaseAlert.bind(this));
+  }
 
   render() {
     for (let i = 0; i < this.product_data.length; i++) {
@@ -32,12 +35,13 @@ class App {
     }
     this.itemArray.forEach((e) => {
       e.updateSelector();
-  
     });
 
-    this.itemArray.forEach(e=>{
+    console.log(this.itemArray);
+
+    this.itemArray.forEach((e) => {
       e.evaluationInsert();
-    })
+    });
   }
 
   createItem() {
@@ -51,57 +55,56 @@ class App {
   // 구매창 쪽 기능
   clickEvent() {
     // itemRepeatCheck
+
     this.itemArray.forEach((element, index) => {
       element.buy.addEventListener("click", () => {
         //이미 선택한 아이템이 있는지 확인  없으면 -1
-        if (this.itemRepeatCheck.indexOf(index) === -1) {     
+        if (this.itemRepeatCheck.indexOf(index) === -1) {
+          this.priceBox.style.display = "block";
+          setTimeout(() => {
+            this.priceBox.style.right = `0px`;
+          }, 10);
+          console.log(this.priceBox.offsetLeft);
           this.itemRepeatCheck.push(element.index);
-          this.purchaseData.push(
-            new ProductList(
-              element.click(),
-              this.count
-            )
-          );
-          
+          this.purchaseData.push(new ProductList(element.click(), this.count));
+
           this.createPurchaseList(element.index, this.count);
           this.count++;
-          element.basket.src = './media/basket/basketok.svg'
+          element.basket.src = "./media/basket/basketok.svg";
 
-          this.calcTotalMoney(index,false);
+          this.calcTotalMoney(index, false);
           return;
         }
         //이미 선택한 아이템이 있을 시 실행
-        this.calcTotalMoney(index,true);
-         
-        });
+        this.calcTotalMoney(index, true);
+      });
     });
   }
- 
-  calcTotalMoney(index,increase){
+
+  calcTotalMoney(index, increase) {
     //increase 변수는 함수가 시작 됐을때 값을 증가시킬 지 여부
 
     const purchasIndex = this.itemRepeatCheck.indexOf(index);
-    if(increase === true) this.purchaseData[purchasIndex].upClick();
+    if (increase === true) this.purchaseData[purchasIndex].upClick();
     this.priceCalc += this.purchaseData[purchasIndex].price;
     this.totalPrice.textContent = `
     Total : ${this.priceCalc.toLocaleString()} 원
-    `
-    return ;
-}
+    `;
+    return;
+  }
 
-  calcMinusMoney(index,decrease){
+  calcMinusMoney(index, decrease) {
     //decrease 변수는 함수가 시작 됐을때 값을 감소시킬 지 여부
     const purchasIndex = this.itemRepeatCheck.indexOf(index);
-   
-    if(decrease === true) this.purchaseData[purchasIndex].downClick();      
+
+    if (decrease === true) this.purchaseData[purchasIndex].downClick();
 
     this.priceCalc -= this.purchaseData[purchasIndex].price;
     this.totalPrice.textContent = `
     Total : ${this.priceCalc.toLocaleString()} 원
-    `
-    return ;
+    `;
+    return;
   }
-
 
   //element Index가 마지막 애를 가리킴
   createPurchaseList(elementIndex, i) {
@@ -110,91 +113,105 @@ class App {
     this.purchaseData.forEach((element) => {
       element.updateSelector();
       element.down.onpointerdown = () => {
-       
         const purchasIndex = this.itemRepeatCheck.indexOf(element.index);
-        if(this.purchaseData[element.index].num <= 0){
+        if (this.purchaseData[element.index].num <= 0) {
           return;
-        } 
- 
+        }
+
         element.downClick();
-        this.calcMinusMoney(elementIndex,false);
+        this.calcMinusMoney(elementIndex, false);
       };
       element.up.onpointerdown = () => {
         element.upClick();
-        this.calcTotalMoney(elementIndex,false);
+        this.calcTotalMoney(elementIndex, false);
       };
     });
   }
 
-  purchaseAlert(){
-   if(this.priceCalc === 0){
-    alert('물건을 담아주십시오');
-    return;
+  purchaseAlert() {
+    if (this.priceCalc === 0) {
+      alert("물건을 담아주십시오");
+      return;
     }
 
-    alert(`${this.priceCalc.toLocaleString()}원 을 결제하시겠습니까?`)
+    alert(`${this.priceCalc.toLocaleString()}원 을 결제하시겠습니까?`);
   }
 }
 
 window.onload = () => {
+  const TESTNUM = 900;
+
   let product_data = [
     {
       image: "./media/product.jfif",
       name: "MacBookPro",
-      star: 4,
+      star: Math.floor(Math.random() * 5) + 0,
       price: 198888,
+      userInfo: Math.floor(Math.random() * TESTNUM) + 0,
     },
     {
       image: "./media/product2.jfif",
       name: "ipad Pro",
-      star: 5,
+      star: Math.floor(Math.random() * 5),
       price: 890000,
+      userInfo: Math.floor(Math.random() * TESTNUM) + 0,
     },
     {
       image: "./media/product3.jfif",
       name: "Apple Watch",
-      star: 2,
+      star: Math.floor(Math.random() * 5),
       price: 600000,
-    },{
-      image:'./media/product4.jfif',
+      userInfo: Math.floor(Math.random() * TESTNUM) + 0,
+    },
+    {
+      image: "./media/product4.jfif",
       name: "AirPods Max",
-      star:4,
-      price:300000
-    },{
-        image:'./media/product5.jfif',
-        name:'ironman',
-        star:5,
-        price:99
-    },{
-        image:'./media/product5.jfif',
-        name:'ironman',
-        star:5,
-        price:99
-    }
-    ,{
-        image:'./media/product5.jfif',
-        name:'ironman',
-        star:5,
-        price:99
-    }
-    ,{
-        image:'./media/product5.jfif',
-        name:'ironman',
-        star:5,
-        price:99
-    }
-    ,{
-        image:'./media/product5.jfif',
-        name:'ironman',
-        star:5,
-        price:99
-    }
-    ,{
-        image:'./media/product5.jfif',
-        name:'ironman',
-        star:5,
-        price:99
-    }
+      star: Math.floor(Math.random() * 5),
+      price: 300000,
+      userInfo: Math.floor(Math.random() * TESTNUM) + 0,
+    },
+    {
+      image: "./media/product5.jfif",
+      name: "ironman",
+      star: Math.floor(Math.random() * 5),
+      price: 990000,
+      userInfo: Math.floor(Math.random() * TESTNUM) + 0,
+    },
+    {
+      image: "./media/product5.jfif",
+      name: "ironman",
+      star: Math.floor(Math.random() * 5),
+      price: 99921,
+      userInfo: Math.floor(Math.random() * TESTNUM) + 0,
+    },
+    {
+      image: "./media/product5.jfif",
+      name: "ironman",
+      star: Math.floor(Math.random() * 5),
+      price: 99342872,
+      userInfo: Math.floor(Math.random() * TESTNUM) + 0,
+    },
+    {
+      image: "./media/product5.jfif",
+      name: "ironman",
+      star: Math.floor(Math.random() * 5),
+      price: 99,
+      userInfo: Math.floor(Math.random() * TESTNUM) + 0,
+    },
+    {
+      image: "./media/product5.jfif",
+      name: "ironman",
+      star: Math.floor(Math.random() * 5),
+      price: 99,
+      userInfo: Math.floor(Math.random() * TESTNUM) + 0,
+    },
+    {
+      image: "./media/product5.jfif",
+      name: "ironman",
+      star: Math.floor(Math.random() * 5),
+      price: 99,
+      userInfo: Math.floor(Math.random() * TESTNUM) + 0,
+    },
   ];
 
   new App(product_data);
--- mall/js/product.js
+++ mall/js/product.js
@@ -1,18 +1,18 @@
 export class Product {
-    constructor(product, index) {
-      this.image = product.image;
-      this.name = product.name;
-      this.star = product.star;
-      this.price = product.price;
-      this.index = index;
-      this.evaluation = null;
-      this.buy = null;
-      this.basket = null;
-  
-    }
-  
-    update() {
-      let html = `
+  constructor(product, index) {
+    this.image = product.image;
+    this.name = product.name;
+    this.star = product.star;
+    this.price = product.price;
+    this.userInfo = product.userInfo;
+    this.index = index;
+    this.evaluation = null;
+    this.buy = null;
+    this.basket = null;
+  }
+
+  update() {
+    let html = `
           <li class="item">
           <div class="img_wrap">
               <img class="product_img" src="${this.image}">
@@ -21,9 +21,13 @@ export class Product {
               <p class="product_name">
                   ${this.name}
               </p>
-              <div class="evaluation">
-           
-            </div>
+              <div class="info">
+                    <div class="evaluation">
+                    </div>
+                    <p class="user">
+                      ${this.userInfo}명 구매
+                  </p>
+                   </div>
               <div class="product_buy">
                   <p class="price">
                       ${this.price.toLocaleString()} 원
@@ -37,42 +41,38 @@ export class Product {
               </div>
           </div>
       </li>`;
-  
-      return html;
-    }
 
-    evaluationInsert(){
-      console.log(this.star);
-      for(let i =0; i < 5;i++){
-        if(i > this.star){
-          this.evaluation.innerHTML += 
-        `
-        <img src="./media/star/yesStar.svg" alt="">
-        `
-        }else{
-          this.evaluation.innerHTML += `
-          <img src="./media/star/noStar.svg" alt="">
-          `
-        }
+    return html;
+  }
+
+  evaluationInsert() {
+    for (let i = 1; i <= 5; i++) {
+      if (i > this.star) {
+        this.evaluation.innerHTML += `
+        <img src="./media/star/noStar.svg" alt="">
+        `;
+      } else {
+        this.evaluation.innerHTML += `
+          <img src="./media/star/yesStar.svg" alt="">
+          `;
       }
     }
-  
-    //생성되고 난 후 실행
-    updateSelector() {
-      this.buy = document.querySelectorAll(".buy")[this.index];
-      this.basket = document.querySelectorAll(".basket > img")[this.index];
-      this.evaluation = document.querySelectorAll('.evaluation')[this.index];
-    }
-  
-    click(e) {
-      let data = {
-        img: this.image,
-        name: this.name,
-        price: this.price,
-      };
-  
-      return data;
-    }
   }
-  
-  
+
+  //생성되고 난 후 실행
+  updateSelector() {
+    this.buy = document.querySelectorAll(".buy")[this.index];
+    this.basket = document.querySelectorAll(".basket > img")[this.index];
+    this.evaluation = document.querySelectorAll(".evaluation")[this.index];
+  }
+
+  click(e) {
+    let data = {
+      img: this.image,
+      name: this.name,
+      price: this.price,
+    };
+
+    return data;
+  }
+}
