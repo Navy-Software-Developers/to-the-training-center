7a4876d83d97fd3c89578a5024015a25fefbc6f3
enkoding1 <ksjdevelop53@gmail.com>
2022-07-18 12:42:56 +0900

    making

---
+++ job/js/joblist/app.js
@@ -0,0 +1,37 @@
+// import {Mili} from './Mili.js';
+
+const Mili = [
+    {
+        'name':'육군',
+        'img':'./media/army.jfif'
+    },
+    {
+        'name':'해군',
+        'img':'./media/navy.jfif'
+    },
+    {
+        'name':'공군',
+        'img':'./meida/airforce.jpg',
+    },
+    {
+        'name':'해병대',
+        'img':'./media/navy2.png'
+    }
+];
+
+class App{
+    constructor(){
+        window.addEventListener('click',()=>{
+            let xhr = new XMLHttpRequest();
+            xhr.open()
+        })
+    }
+}
+
+
+
+
+
+window.onload = ()=>{
+    new App();
+}
