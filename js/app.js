a763d01be75ce3e9dc6e5834f4f0f968bc3519ac
enkoding1 <83891076+hance0103@users.noreply.github.com>
2022-07-21 15:32:46 +0900

    test

--- js/app.js
+++ js/app.js
@@ -130,14 +130,15 @@ window.onload = () => {
 
   function setCookie(name, value, options = {}) {
     options = {
-      path: '/',
+      path: "/",
       // 필요한 경우, 옵션 기본값을 설정할 수도 있습니다.
-      ...options
+      ...options,
     };
     if (options.expires instanceof Date) {
       options.expires = options.expires.toUTCString();
     }
-    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+    let updatedCookie =
+      encodeURIComponent(name) + "=" + encodeURIComponent(value);
     for (let optionKey in options) {
       updatedCookie += "; " + optionKey;
       let optionValue = options[optionKey];
@@ -147,60 +148,62 @@ window.onload = () => {
     }
     document.cookie = updatedCookie;
   }
-  
-  
+
   function getCookie(name) {
-    let matches = document.cookie.match(new RegExp(
-      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
+    let matches = document.cookie.match(
+      new RegExp(
+        "(?:^|; )" +
+          name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
+          "=([^;]*)"
+      )
+    );
     return matches ? decodeURIComponent(matches[1]) : undefined;
   }
-  
+
   let url_prefix = "http://api.xn--o39a35bw4ff5gp5m354a.xn--3e0b707e:8000";
   let verify_url = url_prefix + "/api/accounts/token/verify/";
-  
-  
+
   // POST 메서드 구현 예제
-  async function postData(url = '', data = {}) {
+  async function postData(url = "", data = {}) {
     // 옵션 기본 값은 *로 강조
     const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE 등
+      method: "POST", // *GET, POST, PUT, DELETE 등
       // mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'include', // include, *same-origin, omit
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "include", // include, *same-origin, omit
       headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + getCookie('my-app-auth'),
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + getCookie("my-app-auth"),
         // 'Content-Type': 'application/x-www-form-urlencoded',
       },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
       body: JSON.stringify(data), // body의 데이터 유형은 반드시 "Content-Type" 헤더와 일치해야 함
     });
     return response.json(); // JSON 응답을 네이티브 JavaScript 객체로 파싱
   }
-  
-  
+
   // 로그인 여부 확인
-  let login_btn = document.querySelector('#login_btn');
-  postData(verify_url, { token: getCookie('my-app-auth') }).then((result) => {
-    if(result.code){
+  let login_btn = document.querySelector("#login_btn");
+  postData(verify_url, { token: getCookie("my-app-auth") }).then((result) => {
+    if (result.code) {
       login_btn.innerHTML = "로그인";
       login_btn.href = "./login";
-    }else{
-      login_btn.innerHTML = "로그아웃";
-      login_btn.removeAttribute('href');
-      login_btn.setAttribute('onclick', 'logout()')
+    } else {
+      login_btn.textContent = "로그아웃";
+      login_btn.removeAttribute("href");
+      login_btn.onclick = () => {
+        // logout();
+        alert("hello");
+      };
     }
   });
-  
-  
+
   function logout() {
-    setCookie('my-app-auth', '');
+    setCookie("my-app-auth", "");
   }
-  
+
   // login_btn.onclick = ()=>{
   //   alert('hel');
   // }
-   
 };
