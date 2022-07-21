8be93a77774654f5071bf297a9974891905dd606
enkoding1 <83891076+hance0103@users.noreply.github.com>
2022-07-21 16:32:57 +0900

    update

--- job/js/job/app.js
+++ job/js/job/app.js
@@ -1,39 +1,42 @@
 let chart = document.getElementById("myChart");
 let chart2 = document.getElementById("myChart2");
-const table = document.querySelector('.table'); 
+const table = document.querySelector(".table");
 
 function getCookie(name) {
-  let matches = document.cookie.match(new RegExp(
-    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-  ));
+  let matches = document.cookie.match(
+    new RegExp(
+      "(?:^|; )" +
+        name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
+        "=([^;]*)"
+    )
+  );
   return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
 // POST 메서드 구현 예제
-async function postData(url = '', data = {}) {
+async function postData(url = "", data = {}) {
   // 옵션 기본 값은 *로 강조
   const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE 등
+    method: "POST", // *GET, POST, PUT, DELETE 등
     // mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'include', // include, *same-origin, omit
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "include", // include, *same-origin, omit
     headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + getCookie('my-app-auth'),
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + getCookie("my-app-auth"),
       // 'Content-Type': 'application/x-www-form-urlencoded',
     },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     body: JSON.stringify(data), // body의 데이터 유형은 반드시 "Content-Type" 헤더와 일치해야 함
   });
   return response.json(); // JSON 응답을 네이티브 JavaScript 객체로 파싱
 }
 
-
 window.onload = () => {
   let pk = location.hash.replace("#", "");
-  if(pk == ''){
-    location.href = '/';
+  if (pk == "") {
+    location.href = "/";
   }
   let url_prefix = "http://api.xn--o39a35bw4ff5gp5m354a.xn--3e0b707e:8000";
   let BRANCH = [, "육군", "해군", "공군", "해병대"];
@@ -75,7 +78,7 @@ window.onload = () => {
     //관심목록 유무 토글 변경
   }
 
-  function toggle_like() { }
+  function toggle_like() {}
 
   function draw() {
     console.log(data);
@@ -88,10 +91,8 @@ window.onload = () => {
     let total = [];
     let pass = [];
     let competition = [];
-  
-    let table_data = [];
 
-   
+    let table_data = [];
 
     for (let info of data.recurits) {
       let day = info.enlistStart.toString();
@@ -106,23 +107,25 @@ window.onload = () => {
       let startDay = info.recuritStart.toString();
       let endDay = info.recuritEnd.toString();
 
-      table_data.push(
-        {
-          'round':info.recuritRound,
-          'recurit':info.recuritCnt,
-          'applyed':info.applyedCnt,
-          'start':`${startDay.slice(0,4)}/${startDay.slice(4,6)}/${startDay.slice(6,8)}`,
-          'end':`${endDay.slice(0,4)}/${endDay.slice(4,6)}/${endDay.slice(6,8)}`,
-          'enlistMilitaryUnit':info.enlistMilitaryUnit,
-          'recuritStart': `${year}/${day.slice(4,6)}`,
-          'compete':info.applyedCnt / info.recuritCnt
-        }
-      )
+      table_data.push({
+        round: info.recuritRound,
+        recurit: info.recuritCnt,
+        applyed: info.applyedCnt,
+        start: `${startDay.slice(0, 4)}/${startDay.slice(
+          4,
+          6
+        )}/${startDay.slice(6, 8)}`,
+        end: `${endDay.slice(0, 4)}/${endDay.slice(4, 6)}/${endDay.slice(
+          6,
+          8
+        )}`,
+        enlistMilitaryUnit: info.enlistMilitaryUnit,
+        recuritStart: `${year}/${day.slice(4, 6)}`,
+        compete: info.applyedCnt / info.recuritCnt,
+      });
     }
 
- 
-
-    function render(table_data){
+    function render(table_data) {
       let html = `
       <ul class="row_box">
       <li class="row">${table_data.round}차</li>
@@ -133,16 +136,15 @@ window.onload = () => {
       <li class="row">${table_data.enlistMilitaryUnit}</li>
       <li class="row">${table_data.recuritStart}</li>
     </ul>
-      `
+      `;
       return html;
     }
 
-    for(let t of table_data){
+    for (let t of table_data) {
       table.innerHTML += render(t);
     }
-    
-    
- //   console.log(total, pass);
+
+    //   console.log(total, pass);
     let member = [
       {
         total: total,
@@ -244,85 +246,65 @@ window.onload = () => {
     new Chart(chart, config);
   }
 
-
-
-
-
-
-
-
-  const wiki = document.querySelector('.wiki');
-  const save_wiki = document.querySelector('.save_wiki');
-  const wiki_box = document.querySelector('.wiki_type_box');
-  const wiki_command = document.querySelector('.wiki_command')
-  const wiki_content = document.querySelector('#wiki_content')
+  const wiki = document.querySelector(".wiki");
+  const save_wiki = document.querySelector(".save_wiki");
+  const wiki_box = document.querySelector(".wiki_type_box");
+  const wiki_command = document.querySelector(".wiki_command");
+  const wiki_content = document.querySelector("#wiki_content");
 
   let wiki_text = null;
-  let wiki_url = `${url_prefix}/api/mos/${pk}/wiki` ;
-
-
+  let wiki_url = `${url_prefix}/api/mos/${pk}/wiki`;
 
   //"위키 작성하기" 버튼을 클릭했을때
   wiki.onclick = () => {
-    wiki_box.style.display = 'block';
-    wiki.style.display = 'none';
-    wiki_content.style.display = 'none';
+    wiki_box.style.display = "block";
+    wiki.style.display = "none";
+    wiki_content.style.display = "none";
     wiki_command.value = data.wiki;
-    
-  }
-
+  };
 
   save_wiki.onclick = () => {
     let wiki_result;
-    wiki_box.style.display = 'none';
-    wiki.style.display = 'flex'
+    wiki_box.style.display = "none";
+    wiki.style.display = "flex";
     let wiki_text = wiki_command.value; //작성한 글 내용 가져오기
-
 
     postData(wiki_url, { content: wiki_text }).then((wiki_result) => {
       console.log(wiki_result); // JSON 데이터가 `data.json()` 호출에 의해 파싱됨
-      if(wiki_result.status == 'success'){
+      if (wiki_result.status == "success") {
         data.wiki = wiki_text;
-        wiki.style.display = 'block';
-        wiki_content.style.display = 'block';
+        wiki.style.display = "flex";
+        wiki_content.style.display = "block";
         load();
-      }else{
+      } else {
         alert("수정 실패 로그인등");
       }
     });
+  };
 
-  }
-
-
-
-
-  const search_bar = document.querySelector('.search_bar')
-  const search_input = document.querySelector('.search_input');
-  const search_close = document.querySelector('.search_close'); //flex
-  const autocomplete = document.querySelector('.autocomplete');
+  const search_bar = document.querySelector(".search_bar");
+  const search_input = document.querySelector(".search_input");
+  const search_close = document.querySelector(".search_close"); //flex
+  const autocomplete = document.querySelector(".autocomplete");
 
   search_bar.onclick = () => {
-    search_bar.style.display = 'none';
-    search_input.style.display = 'block';
-    search_close.style.display = 'flex';
-  }
-
+    search_bar.style.display = "none";
+    search_input.style.display = "block";
+    search_close.style.display = "flex";
+  };
 
   search_close.onclick = () => {
-    search_bar.style.display = 'block';
-    search_input.style.display = 'none';
-    search_close.style.display = 'none';
-    autocomplete.style.display = 'none';
-  }
-
+    search_bar.style.display = "block";
+    search_input.style.display = "none";
+    search_close.style.display = "none";
+    autocomplete.style.display = "none";
+  };
 
   //키보드 입력이 들어올때 마다 일어나는 이벤트
   search_input.onkeydown = () => {
-    //키보드 입력시 발생할 이벤트 작성 
-    autocomplete.style.display = 'block';
-
-
-  }
+    //키보드 입력시 발생할 이벤트 작성
+    autocomplete.style.display = "block";
+  };
 };
 
 // new Chart(document.getElementById("myChart"), {
@@ -364,4 +346,3 @@ window.onload = () => {
 //       }
 //     }
 //   });
-
