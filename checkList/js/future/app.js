c44ff2dec6744d602553362349e23f21e4b0ef16
enkoding1 <ksjdevelop53@gmail.com>
2022-07-22 22:48:15 +0900

    update

--- checkList/js/future/app.js
+++ checkList/js/future/app.js
@@ -1,56 +1,237 @@
-
 //one ~ four , 이병 ~ 병장
 
 const SALARY = {
-    "one":510100,
-    "two":552100,
-    "three":610200,
-    "four":676100
+  one: 510100,
+  two: 552100,
+  three: 610200,
+  four: 676100,
 };
 
 //진급 기간
 let promotion_period = {
-    "one":2,
-    "two":6,
-    "three":6
-    
-}
+  one: 2,
+  two: 6,
+  three: 6,
+};
 //육(해병) 해 공 순서  병장기간
-let mili = [4,6,8];
+let mili = [4, 6, 8];
 
 //육, 해 , 공 순서로 최대 넣을수 있는기간으로 표시
-let saving = [18,18,22];
+let saving = [18, 18, 22];
 
-let work = [18,20,22]// 복무기간
+let work = [18, 20, 22]; // 복무기간
 
-
-
-const salary_info = document.querySelectorAll('.salary');
+const salary_info = document.querySelectorAll(".salary");
 
 salary_info[0].textContent = `${SALARY.one.toLocaleString()}원`;
 salary_info[1].textContent = `${SALARY.two.toLocaleString()}원`;
 salary_info[2].textContent = `${SALARY.three.toLocaleString()}원`;
 salary_info[3].textContent = `${SALARY.four.toLocaleString()}원`;
 
-const saving_sel = document.querySelector('.saving_sel');
+const saving_sel = document.querySelector(".saving_sel");
 let save_price = 0;
 
-saving_sel.onclick = ()=>{
-    if(saving_sel.value === ''){
-        return ;
-    }
+saving_sel.onclick = () => {
+  if (saving_sel.value === "") {
+    return;
+  }
 
-    save_price = Number(saving_sel.value);
-}
+  save_price = Number(saving_sel.value);
+};
 
-
-
-const my_salary = document.querySelector('.my_salary');
-const total_salary = document.querySelector('.total_salary');
-const money_price = document.querySelector('.money_price');
-
+const my_salary = document.querySelector(".my_salary");
+const total_salary = document.querySelector(".total_salary");
+const money_price = document.querySelector(".money_price");
 
 // 1 ,2 ,3  육 해 공
-function mySalary(pk){
- 
-}
+function mySalary(pk) {}
+
+const graph = document.querySelector("#graph");
+//2개월 6개월  6개월
+let mili_work = [
+  "1개월",
+  "2개월",
+  "3개월",
+  "4개월",
+  "5개월",
+  "6개월",
+  "7개월",
+  "8개월",
+  "9개월",
+  "10개월",
+  "11개월",
+  "12개월",
+  "13개월",
+  "14개월",
+  "15개월",
+  "16개월",
+  "17개월",
+  "18개월",
+];
+
+let one_mili = 510100 * 2;
+let two_mili = 552100 * 6;
+let three_mili = 610200 * 6;
+let four_mili = 676100 * 4;
+
+let saving_money = 400000;
+
+let money = [
+  510100,
+  510100 * 2,
+
+  552100 + one_mili,
+  552100 * 2 + one_mili,
+  552100 * 3 + one_mili,
+  552100 * 4 + one_mili,
+  552100 * 5 + one_mili,
+  552100 * 6 + one_mili,
+
+  610200 * 1 + one_mili + two_mili,
+  610200 * 2 + one_mili + two_mili,
+  610200 * 3 + one_mili + two_mili,
+  610200 * 4 + one_mili + two_mili,
+  610200 * 5 + one_mili + two_mili,
+  610200 * 6 + one_mili + two_mili,
+
+  676100 * 1 + one_mili + two_mili + three_mili,
+  676100 * 2 + one_mili + two_mili + three_mili,
+  676100 * 3 + one_mili + two_mili + three_mili,
+  676100 * 4 + one_mili + two_mili + three_mili,
+];
+
+let s_one_mili = (510100 - 40) * 2;
+let s_two_mili = (552100 - 40) * 6;
+let s_three_mili = (610200 - 40) * 6;
+let s_four_mili = (676100 - 40) * 4;
+
+let save_money2 = [
+  110100,
+  110100 * 2,
+
+  152100 + s_one_mili,
+  152100 * 2 + s_one_mili,
+  152100 * 3 + s_one_mili,
+  152100 * 4 + s_one_mili,
+  152100 * 5 + s_one_mili,
+  152100 * 6 + s_one_mili,
+
+  210200 * 1 + s_one_mili + s_two_mili,
+  210200 * 2 + s_one_mili + s_two_mili,
+  210200 * 3 + s_one_mili + s_two_mili,
+  210200 * 4 + s_one_mili + s_two_mili,
+  210200 * 5 + s_one_mili + s_two_mili,
+  210200 * 6 + s_one_mili + s_two_mili,
+
+  276100 * 1 + s_one_mili + s_two_mili + s_three_mili,
+  276100 * 2 + s_one_mili + s_two_mili + s_three_mili,
+  276100 * 3 + s_one_mili + s_two_mili + s_three_mili,
+  (33 / 100) *
+    (276100 * 4 +
+      s_one_mili +
+      s_two_mili +
+      s_three_mili +
+      (5 / 100) * (276100 * 4 + s_one_mili + s_two_mili + s_three_mili)) +
+    (276100 * 4 +
+      s_one_mili +
+      s_two_mili +
+      s_three_mili +
+      (5 / 100) * (276100 * 4 + s_one_mili + s_two_mili + s_three_mili)),
+];
+let save_money = [
+  400000,
+  400000 * 2,
+  400000 * 3,
+  400000 * 4,
+  400000 * 5,
+  400000 * 6,
+  400000 * 7,
+  400000 * 8,
+  400000 * 9,
+  400000 * 10,
+  400000 * 11,
+  400000 * 12,
+  400000 * 13,
+  400000 * 14,
+  400000 * 15,
+  400000 * 16,
+  400000 * 17,
+  (33 / 100) * ((5 / 100) * (400000 * 18) + 400000 * 18) + 400000 * 18,
+];
+console.log(money.length);
+
+let config = {
+  type: "line",
+  // ②차트의 데이터(Object)
+  data: {
+    // ③x축에 들어갈 이름들(Array)
+    labels: mili_work,
+    // ④실제 차트에 표시할 데이터들(Array), dataset객체들을 담고 있다.
+    datasets: [
+      {
+        // ⑤dataset의 이름(String)
+        label: "월급",
+        // ⑥dataset값(Array)
+        data: money,
+        // ⑦dataset의 배경색(rgba값을 String으로 표현)
+        backgroundColor: "rgba(244,0,233,0.2)",
+        // ⑧dataset의 선 색(rgba값을 String으로 표현)
+        borderColor: "red",
+        // ⑨dataset의 선 두께(Number)
+        borderWidth: 1,
+      },
+      {
+        // ⑤dataset의 이름(String)
+        label: "적금만",
+        // ⑥dataset값(Array)
+        data: save_money,
+        // ⑦dataset의 배경색(rgba값을 String으로 표현)
+        backgroundColor: "rgba(10,255,0,0.2)",
+        // ⑧dataset의 선 색(rgba값을 String으로 표현)
+        borderColor: "green",
+        // ⑨dataset의 선 두께(Number)
+        borderWidth: 1,
+      },
+      {
+        // ⑤dataset의 이름(String)
+        label: "월급 + 적금",
+        // ⑥dataset값(Array)
+        data: save_money2,
+        // ⑦dataset의 배경색(rgba값을 String으로 표현)
+        backgroundColor: "rgba(0,0,255,0.6)",
+        // ⑧dataset의 선 색(rgba값을 String으로 표현)
+        borderColor: "blue",
+        // ⑨dataset의 선 두께(Number)
+        borderWidth: 1,
+      },
+    ],
+  },
+  // ⑩차트의 설정(Object)
+  options: {
+    // ⑪축에 관한 설정(Object)
+    scales: {
+      // ⑫y축에 대한 설정(Object)
+      y: {
+        // ⑬시작을 0부터 하게끔 설정(최소값이 0보다 크더라도)(boolean)
+        beginAtZero: true,
+      },
+    },
+  },
+};
+
+window.onload = () => {
+  new Chart(graph, config);
+
+  let save_m = 276100 * 4 + s_one_mili + s_two_mili + s_three_mili;
+  let save_per = (5 / 100) * save_m;
+
+  let mysal = document.querySelector(".my_salary");
+  let static_money = document.querySelector(".money_price");
+  let total_sal = document.querySelector(".total_salary");
+  let korea = document.querySelector(".korea_add");
+
+  mysal.textContent = money[17].toLocaleString();
+  total_sal.textContent = (save_per + save_m).toLocaleString();
+  korea.textContent = save_money2[17].toLocaleString();
+  static_money.textContent = (save_money2[17] - money[17]).toLocaleString();
+};
