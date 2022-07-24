b5b593e87ab74c7fb6eccd457b0ce6df7a65a98b
enkoding1 <ksjdevelop53@gmail.com>
2022-07-24 22:30:57 +0900

    update

--- checkList/js/future/app.js
+++ checkList/js/future/app.js
@@ -1,60 +1,14 @@
 //one ~ four , 이병 ~ 병장
 
-const SALARY = {
-  one: 510100,
-  two: 552100,
-  three: 610200,
-  four: 676100,
-};
-
-//진급 기간
-let promotion_period = {
-  one: 2,
-  two: 6,
-  three: 6,
-};
-
 const salary_list = [
   510100, 510100, 552100, 552100, 552100, 552100, 552100, 552100, 610200,
   610200, 610200, 610200, 610200, 610200, 676100, 676100, 676100, 676100,
 ];
 
-//육(해병) 해 공 순서  병장기간
-let mili = [4, 6, 8];
-
-//육, 해 , 공 순서로 최대 넣을수 있는기간으로 표시
-let saving = [18, 18, 22];
-
+let mili = [4, 6, 8]; //육(해병) 해 공 순서  병장기간
+let saving = [18, 18, 22]; //육, 해 , 공 순서로 적금 넣을수 있는기간으로 표시
 let work = [18, 20, 22]; // 복무기간
 
-const salary_info = document.querySelectorAll(".salary");
-
-salary_info[0].textContent = `${SALARY.one.toLocaleString()}원`;
-salary_info[1].textContent = `${SALARY.two.toLocaleString()}원`;
-salary_info[2].textContent = `${SALARY.three.toLocaleString()}원`;
-salary_info[3].textContent = `${SALARY.four.toLocaleString()}원`;
-
-const saving_sel = document.querySelector(".saving_sel");
-let save_price = 0;
-
-saving_sel.onclick = () => {
-  if (saving_sel.value === "") {
-    return;
-  }
-
-  save_price = Number(saving_sel.value);
-};
-
-const my_salary = document.querySelector(".my_salary");
-const total_salary = document.querySelector(".total_salary");
-const support_money = document.querySelector(".suport_money");
-const money_price = document.querySelector(".money_price");
-
-// 1 ,2 ,3  육 해 공
-function mySalary(pk) {}
-
-const graph = document.querySelector("#graph");
-//2개월 6개월  6개월
 let mili_work = [
   "1개월",
   "2개월",
@@ -75,19 +29,6 @@ let mili_work = [
   "17개월",
   "18개월",
 ];
-
-let total = [];
-let sum = 0;
-let mili_pk = 18;
-
-for (let i = 0; i < mili_pk; i++) {
-  if (mili_pk == 18) {
-    sum += salary_list[i];
-    total.push(sum);
-  }
-}
-
-console.log(sum);
 
 let config = {
   type: "line",
